e34aafd5a804ec161cc6afda83ef974a8eb0b5fc
Dima Shlonsky <dima@toptix.co.il>
2016-06-05 17:18:25 +1000

    Added ItemExtraDataDlg-AutoClear

---
+++ src/js/ItemExtraDataDlg-AutoClear.js
@@ -0,0 +1,7 @@
+﻿$(basketValidationPanel).bind("itemsExtraDataDialog.show.esro", function (e, container) {
+    $("#btnNext, #btnPrev", container).click(function () {
+        if ($(".basketManRow.selected", container).is(".missing")) {
+            $(".dataField:visible", container).val("");
+        }
+    });
+});
