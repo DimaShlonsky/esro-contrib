1af26533c63aa6c6f987a0018cfe83f86681f535
Dima Shlonsky <dima@toptix.co.il>
2016-06-05 17:21:22 +1000

    Added comment

--- src/js/ItemExtraDataDlg-AutoClear.js
+++ src/js/ItemExtraDataDlg-AutoClear.js
@@ -1,4 +1,5 @@
-﻿$(basketValidationPanel).bind("itemsExtraDataDialog.show.esro", function (e, container) {
+﻿//causes the Items Extra Data form in the basket page to be cleared when moving between the basket items
+$(basketValidationPanel).bind("itemsExtraDataDialog.show.esro", function (e, container) {
     $("#btnNext, #btnPrev", container).click(function () {
         if ($(".basketManRow.selected", container).is(".missing")) {
             $(".dataField:visible", container).val("");
