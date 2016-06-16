17b0c4b834c2b05ee7708b3c4f52bbc744aec88f
Dima Shlonsky <dima@toptix.co.il>
2016-06-16 21:25:44 +1000

    Added customization SelectTickets-CustomPriceTypeLimitError.js

---
+++ src/js/SelectTickets-CustomPriceTypeLimitError.js
@@ -0,0 +1,10 @@
+﻿//Replaces the LimitPriceTypePerEvent error message with a custom one saying "<price type> SOLD OUT"
+$eSRO.api.bind("afterSetAreaTickets.TicketingController", function (e, status, data, errorThrown, xhr) {
+    if (status == "error") {
+        var err = JSON.parse(xhr.responseText);
+        if (err.type == "OperationResultException" && err.faultCode == "LimitPriceTypePerEvent1" && err.faultDetail.Available == 0) {
+            err.message = err.faultDetail.PriceType + " SOLD OUT";
+            xhr.responseText = JSON.stringify(err);
+        }
+    }
+});
