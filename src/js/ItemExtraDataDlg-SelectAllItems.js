bfd63bef9eeb66720604a3d7337d3a91593a362f
Dima Shlonsky <dima@toptix.co.il>
2016-06-16 21:58:47 +1000

    Added customization ItemExtraDataDlg-SelectAllItems.js

---
+++ src/js/ItemExtraDataDlg-SelectAllItems.js
@@ -0,0 +1,13 @@
+﻿//makes all the items in the itemExtraDataDialof selected by default, allowing to fill the form for all the items at the same time
+if (typeof (basketValidationPanel) != "undefined") {
+	$(basketValidationPanel).bind("itemsExtraDataDialog.show.esro", function(e, container){
+		$(".basketManRow>*", container).click(function(e){ e.stopPropagation(); });
+		$("input.allSelector", container).each(function(i,e){
+			var inp = $(this);
+			inp.attr("checked", true);
+			inp.click();
+			inp.attr("checked", true);
+			//setCheckboxGroup(inp);
+		});
+	});
+}
