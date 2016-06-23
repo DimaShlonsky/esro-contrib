6dde8fda838880065dbad591c5fdaa83f06d7970
Dima Shlonsky <dima@toptix.co.il>
2016-06-23 15:19:14 +1000

    Improved SelectTickets-AutoMultiplyPriceType ; made it display multiplied price

--- src/js/SelectTickets-AutoMultiplyPriceType.js
+++ src/js/SelectTickets-AutoMultiplyPriceType.js
@@ -1,9 +1,13 @@
 ﻿//Modifies the behavior of SelectTickets control in GA mode for a certain price type, causing it to add multiple tickets for each ticket added
 define(['jquery'], function ($) {
     return (function autoMultiplyPriceType(priceTypeId, multiplier) {
-        $(".selectTickets.mode-GeneralAdmission").bind("init.esro.selectTickets", function (event) {
-            var inputCount = $(".row:has(.priceTypes[value='" + priceTypeId + "']) .count", this);
+        $(".selectTickets.mode-GeneralAdmission, .selectTickets.mode-BestAvailable").bind("init.esro.selectTickets", function (event) {
+            var row = $(".row:has(.priceTypes[value='" + priceTypeId + "'])", this)
+                inputCount = row.find(".count");
             if (inputCount.length > 0) {
+                var priceLevel = $("#selPriceLevel", this).val(),
+                    price = selectTicketsControl.pricing.areaPricing[priceTypeId].priceLevels[priceLevel].fullPrice;
+                row.find(".pricing > .price").text(FormatCurrency(price * multiplier));
                 inputCount.hide();
                 if (inputCount.is(".numericSpinner.ui-spinner-input")) {
                     inputCount.spinner("destroy");
