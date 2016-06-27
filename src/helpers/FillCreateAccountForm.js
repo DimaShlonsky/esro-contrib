7e7c85e5e8702ffad1d399327f0e89c6ff033c4b
Dima Shlonsky <dima@toptix.co.il>
2016-06-27 18:38:46 +1000

    Made FillCreateAccount helper use random values

--- src/helpers/FillCreateAccountForm.js
+++ src/helpers/FillCreateAccountForm.js
@@ -1,11 +1,45 @@
-﻿(function (n) {
-    $.each(["FirstName", "LastName", "MiddleName", "NamePrefix", "NameSuffix", "Initials", "AccountName", "TaxExemptNumber", "VatRegistrationNumber", "Phone", "Phone2", "Phone3", "Password", "Address", "Address2", "City", "Country", "ZipCode", "IDNumber", "Age", "State", "Address3", "Fax", "Website"]
-	, function (k, v) {
-	    $("[name=" + v + "]").val(n);
-	});
-    $("[name=Email]").val(n + "@localhost");
-    $("[name=Password]").val("aA12345!");
-    $("select[name]").each(function (i, e) {
-        $(this).val($(this).find("option[value]:not([value='']):eq(0)").val());
-    });
-})((new Date()).valueOf().toString().substr(5));
+﻿define(['https://rawgit.com/chancejs/chancejs/master/dist/chance.min.js'], function (Chance) {
+    return function(){
+        var chance = new Chance(),
+            fields = {"FirstName":chance.first(),
+                "Email":chance.email()+".example.com",
+                "LastName":chance.last(),
+                "MiddleName":"",
+                "NamePrefix":chance.name_prefix(),
+                "NameSuffix":chance.name_suffix(),
+                "TaxExemptNumber":chance.natural(),
+                "VatRegistrationNumber":chance.natural(),
+                "Phone":chance.phone(), 
+                "Phone2":chance.phone(),
+                "Phone3":chance.phone(),
+                "Password":"aA12345!",
+                "Address": chance.address(),
+                "Address2":"",
+                "Address3":"",
+                "City":chance.city(),
+                "Country":chance.country(),
+                "ZipCode":chance.zip(),
+                "IDNumber":chance.israelId(),
+                "Age":chance.age(),
+                "State":chance.state(), 
+                "Fax": chance.phone(),
+                "Website":chance.url()
+            };
+        fields.AccountName = fields.Website;
+        fields.Initials = fields.FirstName.substr(0, 1).toUpperCase() + fields.LastName.substr(0, 1).toUpperCase();
+        fields.PasswordVerify = fields.Password;
+        $.each(fields, function (k, v) {
+            var input = $("[name]").filter(function (i, e) { return $(e).attr("name") == k; });
+            if (input.is("select")) {
+                var option = input.find("option").filter(function(i,e){ return like($(e).val(), v) || like($(e).text(), v);});
+                if (option.length==0)  option = input.find("option[value]:not([value='']):eq(0)");
+                input.val(option.val());
+            } else {
+                input.val(v);
+            }
+        });
+    }
+    function like(a,b){
+        return $.trim(a).toLowerCase() == $.trim(b).toLowerCase();
+    }
+});
