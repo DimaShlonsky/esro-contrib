7e64fd269759c4d69c00d3c9b054b652c24f9c38
Dima Shlonsky <dima@toptix.co.il>
2017-02-23 17:24:18 +1100

    Added SeatingPlan-ConfirmationOnClick

---
+++ src/js/SeatingPlan-ConfirmationOnClick.js
@@ -0,0 +1,57 @@
+﻿/* Shows an alert dialog when a seat is clicked, requiring to click "ok" before the ticket is added
+    Example, assuming the file is at /custom: 
+        require.config({
+            config:{
+                'custom/SeatingPlan-ConfigurationOnClick':{
+                    txtAlert:"Alert!",
+                    txtTitle:"Title here",
+                    txtOk: "Accept",
+                    txtCancel: "Reject"
+                }
+            }
+        });
+        require(['custom/SeatingPlan-ConfigurationOnClick']);
+*/
+define(['module', 'js/SeatingPlan'], function (module, SP) {
+    var cfg = module.config()
+    SP.getInstancePromise(document)
+        .done(function (spInstance) {
+            return spInstance.getLoadedPromise();
+        })
+        .done(function (spInstance) {
+            $("img.seat", spInstance.seatsContainer).on("seatClicked.esro", function (e, seat) {
+                var me = this, args = $.makeArray(arguments);
+                if (e.skipSeatInfoCheck || !seat.info || $(me).data("alertAccepted") || seat.status != 10) {
+                    return;
+                }
+                e.result = false;
+                e.stopImmediatePropagation();
+                $("<div>",
+                    { text: cfg.txtAlert || seat.info }
+                ).dialog({
+                    title:(cfg.txtTitle),
+                    buttons: [
+                        {
+                            text:(cfg.txtOk || "ok"),
+                            click: function () {
+                                $(this).dialog("close");
+                                $(me).data("alertAccepted", true);
+                                proceed();
+                            }
+                        },
+                        {
+                            text: (cfg.txtCancel || "cancel"),
+                            click: function () {
+                                $(this).dialog("close")
+                            }
+                        }
+                    ]
+                });
+                function proceed() {
+                    var e = $.Event("seatClicked.esro");
+                    e.skipSeatInfoCheck = true;
+                    $(me).trigger(e, args.slice(1))
+                }
+            })
+        });
+});
